863a0aef8d4664c96df0016a453c62bc6e7430dd
liudoudou86 <543972930@qq.com>
2022-08-10 18:45:31 +0800

    feat：同步更新配料表

--- uniCloud-aliyun/cloudfunctions/updateMorningMaterialsData/index.js
+++ uniCloud-aliyun/cloudfunctions/updateMorningMaterialsData/index.js
@@ -5,45 +5,89 @@ exports.main = async (event, context) => {
 	// 初始化数据库
 	const db = uniCloud.database();
 	if (event.type === 'morningfood') {
-		console.log('旧数据',event.other);
+		// 通过修改名字获取新的配料表
+		const newValue = await db.collection('cooking').where({
+			nickname : event.nickname
+		}).get();
+		const newData = newValue.data[0].materials;
 		// 通过条件更新客户端的数据
 		const res = await db.collection('morningmenu').where({
 			date : event.date
 		}).update({
 			morningfood : event.nickname
 		});
-		console.log("更新数据", res)
+		// 通过旧的名字更新表中所有数据
+		const old = await db.collection('morningmaterials').where({
+			name : event.morningfood
+		}).update({
+			name : event.nickname,
+			text : newData
+		});
+		console.log("更新数据", res);
 		//返回数据给客户端
 		return res
 	}else if (event.type === 'morningsoup') {
-		console.log('旧数据',event.other);
+		// 通过修改名字获取新的配料表
+		const newValue = await db.collection('cooking').where({
+			nickname : event.nickname
+		}).get();
+		const newData = newValue.data[0].materials;
 		// 通过条件更新客户端的数据
 		const res = await db.collection('morningmenu').where({
 			date : event.date
 		}).update({
 			morningsoup : event.nickname
 		});
+		// 通过旧的名字更新表中所有数据
+		const old = await db.collection('morningmaterials').where({
+			name : event.morningsoup
+		}).update({
+			name : event.nickname,
+			text : newData
+		});
 		console.log("更新数据", res)
 		//返回数据给客户端
 		return res
 	} else if (event.type === 'fruit') {
-		console.log('旧数据',event.other);
+		// 通过修改名字获取新的配料表
+		const newValue = await db.collection('cooking').where({
+			nickname : event.nickname
+		}).get();
+		const newData = newValue.data[0].materials;
 		// 通过条件更新客户端的数据
 		const res = await db.collection('morningmenu').where({
 			date : event.date
 		}).update({
 			fruit : event.nickname
 		});
+		// 通过旧的名字更新表中所有数据
+		const old = await db.collection('morningmaterials').where({
+			name : event.fruit
+		}).update({
+			name : event.nickname,
+			text : newData
+		});
 		console.log("更新数据", res)
 		//返回数据给客户端
 		return res
 	} else if (event.type === 'other') {
-		console.log('旧数据',event.other);
+		// 通过修改名字获取新的配料表
+		const newValue = await db.collection('cooking').where({
+			nickname : event.nickname
+		}).get();
+		const newData = newValue.data[0].materials;
 		// 通过条件更新客户端的数据
 		const res = await db.collection('morningmenu').where({
 			date : event.date
 		}).update({
 			other : event.nickname
+		});
+		// 通过旧的名字更新表中所有数据
+		const old = await db.collection('morningmaterials').where({
+			name : event.other
+		}).update({
+			name : event.nickname,
+			text : newData
 		});
 		console.log("更新数据", res)
 		//返回数据给客户端
